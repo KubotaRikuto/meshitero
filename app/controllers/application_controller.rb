3d7b5bcf94bbda9cd0103bdb51c01557eaf6045a
KubotaRikuto <114932185+KubotaRikuto@users.noreply.github.com>
2022-10-31 18:17:58 +0000

    [Finish] 4-7 sign in,sign up,sign out実装また、遷移後に表示するviewの設定も実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+  # before_actionでdevise機能利用前に"configure_permitted_parameters"メソッドが実行される
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+    # サインイン後の遷移先を設定
+    def after_sign_in_path_for(resource)
+      about_path
+    end
+
+    # サインアウト後の遷移先を設定
+    def after_sign_out_path_for(resource)
+      about_path
+    end
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'homes/about' => 'homes#about', as: 'about'
+
 end
