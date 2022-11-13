be76a7c120b8c69597cad914145899d952df6811
KubotaRikuto <114932185+KubotaRikuto@users.noreply.github.com>
2022-11-13 23:26:22 +0000

    [Add] 4-24 BootStrap追加,他のユーザーからのアクセス制限追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -18,6 +21,14 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if (user_id != login_user_id)
+      redirect_to post_images_path
+    end
   end
 
 end
